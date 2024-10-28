0c6e2d61fed70af1032efa3d4ad253218d7443c5
hye0ni <shyeon0812@gmail.com>
2024-10-28 12:23:37 +0900

    hello flask 데이터 전달까지

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-from flask import Flask
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def hello():
     return 'Hello, World!'
 
-    @app.route('/baseball')
-def baseball():
-    return 'Hello, baseball!'
+@app.route('/naver')
+def naver():
+    return render_template('naver.html')
 
-    @app.route('/method',methods=['GET', 'POST'])
+@app.route('/method', methods=['GET', 'POST'])
 def method():
-        if request.method == 'GET':
-            return "GET으로 전달"
-         else:
-                return "POST로 전달"
+    if request.method == 'GET':
+        return "GET으로 전달"
+    else:
+        keyword = request.form["keyword"]
+        print(keyword)
+        return f"POST로 전달된 당신이 입력한 검색어: {keyword}"
 
-    if __name__ == '__main__':
-    app.run()
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
